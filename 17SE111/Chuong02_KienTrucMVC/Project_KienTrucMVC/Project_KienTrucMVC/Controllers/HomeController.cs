83e0493d46c9ae1a57c2b636adae4c79cb54fd54
NguyenMinhPhuc <phuc@lhu.edu.vn>
2020-03-13 15:41:25 +0700

    Cập nhật nội dung project chương 02

---
+++ 17SE111/Chuong02_KienTrucMVC/Project_KienTrucMVC/Project_KienTrucMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Project_KienTrucMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_KienTrucMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection f)
+        {
+            SinhVien sv = new SinhVien();
+            sv.SVID = Convert.ToInt32(f["txtSVID"]);
+            sv.SVTen = f["txtSVTen"];
+            sv.SVDiaChi = f["txtSVDiaChi"];
+            return View(sv);
+        }
+    }
+}
---
+++ 17SE111/Chuong02_KienTrucMVC/Project_KienTrucMVC/Project_KienTrucMVC/Models/SinhVien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_KienTrucMVC.Models
+{
+    public class SinhVien
+    {
+        public int SVID { get; set; }
+        public string SVTen { get; set; }
+        public string SVDiaChi { get; set; }
+    }
+}
